4e83c6376f2b72594513fbfb7257001775d7a302
Sourabh Sahu <sourabhsahu8976@gmail.com>
2024-10-07 16:02:06 +0530

    program1 done

--- program1.js
+++ program1.js
@@ -3,9 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const pairs = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+
+    for (let char of s) {
+        if (pairs[char]) {
+            stack.push(char);
+        } else {
+            const top = stack.pop();
+            if (pairs[top] !== char) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
--- program2.js
+++ program2.js
@@ -3,8 +3,30 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
+    let result = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        const current = romanMap[s[i]];
+        const next = romanMap[s[i + 1]];
+        
+        if (next && current < next) {
+            result -= current;
+        } else {
+            result += current;
+        }
+    }
+    
+    return result;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
